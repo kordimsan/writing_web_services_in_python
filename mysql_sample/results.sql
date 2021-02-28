d27bdd2d557905db915c9b934b86d6e9ba2a85c9
Dmitry Korzunin <kordimsan@mail.ru>
2021-02-28 21:26:49 +0500

    refactoring

--- mysql_sample/results.sql
+++ mysql_sample/results.sql
@@ -11,31 +11,31 @@ select name from store where is_automated = 1;
 select sum(total) from sale;
 
 -- 4. Получить уникальные store_id всех складов, с которых была хоть одна продажа
-select distinct store_id from sale;
+select distinct(store_id) from sale;
 
 -- 5. Получить уникальные store_id всех складов, с которых не было ни одной продажи
-select store_id from store where store_id not in (SELECT store_id from sale);
+select store_id from store natural left join sale where sale.store_id is null;
 
 -- 6. Получить для каждого товара название и среднюю стоимость единицы товара avg(total/quantity), если товар не продавался, он не попадает в отчет.
-select p.name, avg(total/quantity) from sale as s
-inner join product as p on p.product_id = s.product_id
-group by p.name;
+select name, avg(total/quantity) from product 
+natural join sale 
+group by product_id;
 
 -- 7. Получить названия всех продуктов, которые продавались только с единственного склада
-select p.name from product as p
-inner join sale as s on p.product_id = s.product_id
-group by p.name
-having COUNT(s.store_id) = 1;
+select name from product 
+natural join sale 
+group by product_id
+having count(distinct store_id) = 1;
 
 -- 8. Получить названия всех складов, с которых продавался только один продукт
-select p.name from store as p
-inner join sale as s on p.store_id = s.store_id
-group by p.name
-having COUNT(s.sale_id) = 1;
+select name from store 
+natural join sale 
+group by store_id
+having count(distinct product_id) = 1;
 
 -- 9. Выберите все ряды (все поля) из продаж, в которых сумма продажи (total) максимальна (равна максимальной из всех встречающихся)
-select * from sale 
-where total in (select MAX(total) from sale);
+select * from sale
+where total = (select max(total) from sale);
 
 -- 10. Выведите дату самых максимальных продаж, если таких дат несколько, то самую раннюю из них
-select date from sale where total in (select MAX(total) from sale) order by date LIMIT 1;
+select date from sale group by date order by sum(total) DESC, date ASC LIMIT 1;
